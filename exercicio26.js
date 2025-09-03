481d861ee82cd23c7b14ff94bc5861afc190703f
RenBio <rengts.bio@gmail.com>
2025-09-02 22:08:56 -0300

    Add files via upload

---
+++ exercicio26.js
@@ -0,0 +1,30 @@
+<html>
+<head>
+
+
+</head>
+
+<body>
+ <script>
+let i = 1;
+do {
+if (i % 2 != 0){
+    console.log(i);
+}
+i++;
+}while(i<= 20);
+
+
+
+
+
+ </script>   
+
+
+
+
+</body>
+
+
+
+</html>
