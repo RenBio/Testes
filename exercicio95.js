0b0c7f02cda8c0f111b1f7cc5a33cf058853c45a
Renan Trindade <rengts.bio@gmail.com>
2025-10-03 23:00:00 -0300

    Add files via upload

---
+++ exercicio95.js
@@ -0,0 +1,6 @@
+function concatenateArrays(arr1,arr2) {
+    return arr1.concat(arr2);
+}
+const firstArray = [1,2,3];
+const secondArray = [4,5,6];
+console.log(concatenateArrays(firstArray,secondArray)); //[1,2,3,4,5,6]
